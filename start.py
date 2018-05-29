ee3ce1b6b7c12a9a5f67d61187745f5ebdc41360
dsasmita <dangs.work@gmail.com>
2018-05-29 21:27:08 +0700

    Initial commit

---
+++ start.py
@@ -0,0 +1,12 @@
+daftar_teman = []
+daftar_teman.append('Joko')
+daftar_teman.append('Bowo')
+daftar_teman.append('Mega')
+daftar_teman.append('Beye')
+
+
+print ("Daftar Teman:")
+print ("---")
+
+for dn in daftar_teman:
+    print (dn)
